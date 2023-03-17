e389bb72c0da757749bd1077748b44643c1e8d88
pomidorNagolove <harrier136@icloud.com>
2023-03-17 22:03:05 +0300

    save

--- ITMO/v3/podpage1.js
+++ ITMO/v3/podpage1.js
@@ -2,5 +2,4 @@ function SendPost() {
     let input = document.getElementById("Depth");
     let length = input.value.length;
    if (length < 1) alert('Заполните Chem For')
-           else (
-             alert('Все заполнено'))}
+           }
---
+++ ITMO/v3/podpage2.js
@@ -0,0 +1,5 @@
+function SendPost() {
+    let input = document.getElementById("Chemical");
+    let length = input.value.length;
+   if (length == 0) alert('Заполните Chem For')
+           }
