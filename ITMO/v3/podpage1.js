00bba9aa905dd632a67f7024c8d253850fdd49f0
pomidorNagolove <harrier136@icloud.com>
2023-03-17 03:54:51 +0300

    add new pages

---
+++ ITMO/v3/podpage1.js
@@ -0,0 +1,6 @@
+function SendPost() {
+    let input = document.getElementById("Depth");
+    let length = input.value.length;
+   if (length < 1) alert('Заполните Chem For')
+           else (
+             alert('Все заполнено'))}
